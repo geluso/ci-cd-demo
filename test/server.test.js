fb3d14ea3d05d84ee3edb212f8f2dac2f3a089ce
Steve Geluso <stevegeluso@gmail.com>
2018-03-28 18:46:25 -0700

    building simple server test

---
+++ test/server.test.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const request = require('superagent');
+const URL = 'http://localhost:' + process.env.PORT + '/';
+
+describe('Server Responses', () => {
+  it('should return 200 for root', (done) => {
+    request.get(URL)
+    .end((err, res) => {
+      expect(res.status).toEqual(200);
+      done();
+    });
+  });
+
+  it('should contain expected text', (done) => {
+    request.get(URL)
+    .end((err, res) => {
+      let html = 'Welcome! Try to access <a href="/secret">/secret</a> to see a secret recipe!';
+      expect(res.text).toEqual(html);
+      done();
+    });
+  });
+});
