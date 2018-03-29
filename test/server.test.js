fb6598ac81c172ad2852d4a3f6a8f4e2d87079dd
Steve Geluso <stevegeluso@gmail.com>
2018-03-28 18:50:07 -0700

    console logging errors in test

--- test/server.test.js
+++ test/server.test.js
@@ -7,6 +7,9 @@ describe('Server Responses', () => {
   it('should return 200 for root', (done) => {
     request.get(URL)
     .end((err, res) => {
+      if (err) {
+        console.log(err);
+      }
       expect(res.status).toEqual(200);
       done();
     });
@@ -15,6 +18,9 @@ describe('Server Responses', () => {
   it('should contain expected text', (done) => {
     request.get(URL)
     .end((err, res) => {
+      if (err) {
+        console.log(err);
+      }
       let html = 'Welcome! Try to access <a href="/secret">/secret</a> to see a secret recipe!';
       expect(res.text).toEqual(html);
       done();
